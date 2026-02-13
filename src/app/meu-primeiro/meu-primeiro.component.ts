ee2b193eb1f4c8f76c493d97b1efbc4aaf3d0365
GustavoSB3 <161391954+GustavoSB3@users.noreply.github.com>
2026-02-13 06:24:20 -0300

    commit 1

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro',
+  standalone: true,
+  template: `
+    <p>Meu primeiro componente com Angular</p>
+  `
+})
+export class MeuPrimeiroComponent {}
